ce89bec90728f5c86e558d0bac1235bcf022271b
Richard Lamboj <rl@unicom.ws>
2016-06-21 21:47:58 +0200

    will now install Firefox-ESR version 45.2.0

--- config/mozilla.lua
+++ config/mozilla.lua
@@ -1,7 +1,7 @@
 
 do
     MFFESR_IN = [[smb://\\10.0.19.10\software\mffesr.ini]]
-    MFFESR_EXE = "https://download-installer.cdn.mozilla.net/pub/firefox/releases/38.2.0esr/win32/de/Firefox%20Setup%2038.2.0esr.exe"
+    MFFESR_EXE = "https://download-installer.cdn.mozilla.net/pub/firefox/releases/45.2.0esr/win32/en-US/Firefox%20Setup%2045.2.0esr.exe"
     UNINSTALL_EXE = [["%PROGRAMFILES%\Mozilla Firefox\uninstall\helper.exe"]]
     UNINSTALL_EXE = [[file://"%PROGRAMFILES(X86)%\Mozilla Firefox\uninstall\helper.exe"]]
     UNINSTALL_SILENT = "/S"
@@ -68,7 +68,7 @@ do
     upgrade_cmds = {KILL_FF_CMD, INSTALL_CMD}
     uninstall_cmds = {KILL_FF_CMD, UNINSTALL_CMD}
 
-    register_package('firefox_esr', [[Mozilla Firefox]], "38.2.0", "1", is_in_software_list([[Mozilla Firefox [0-9\.]+]]), is_in_software_list([[Mozilla Firefox [0-9\.]+]]), install_cmds, upgrade_cmds, uninstall_cmds, "", {})
+    register_package('firefox_esr', [[Mozilla Firefox]], "45.2.0", "1", is_in_software_list([[Mozilla Firefox [0-9\.]+]]), is_in_software_list([[Mozilla Firefox [0-9\.]+]]), install_cmds, upgrade_cmds, uninstall_cmds, "", {})
 end
 
 function firefox_esr_is_installed ()

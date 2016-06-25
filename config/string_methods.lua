751739cc32b7827ed311f97c6512569fa1fd3dd1
Richard Lamboj <rl@unicom.ws>
2016-06-25 06:01:17 +0200

    added string related samples

---
+++ config/string_methods.lua
@@ -0,0 +1,7 @@
+tmp = split("2.7.5", ".")
+print(tmp[0])
+print(tmp[1])
+print(tmp[2])
+
+print(tostring(startswith("Sarah Kerrigan", "Sarah")))
+print(tostring(endswith("Sarah Kerrigan", "Kerrigan")))

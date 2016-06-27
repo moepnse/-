98cc733565c3a07404a7eadb477dd86e61c88036
Richard Lamboj <rlamboj@purrr.email>
2016-06-27 23:16:18 +0200

    attempt to fix broken install-script (questionable mental state on creation-time?)

--- install.bat
+++ install.bat
@@ -11,13 +11,18 @@ set DEPENDENCIES_SOURCE=pev
 set PI_PATH=%HOMEDRIVE%\pi
 
 echo Installing Dependencies...
-call /i %~dp0dependencies/install.bat
-
-if not PI_PATH == %~dp0 (
-    if exist PI_PATH (
-        move PI_PATH PI_PATH_%date%
+call %~dp0dependencies\install_dependencies.bat
+if not %PI_PATH% == %~dp0 (
+    if exist %PI_PATH% (
+        set PI_BACKUP_DIR=%PI_PATH%_%date%
+        if exist %PI_BACKUP_DIR% (
+            REM    /S      Removes all directories and files in the specified directory in addition to the directory itself. Used to remove a directory tree.
+            REM    /Q      Quiet mode, do not ask if ok to remove a directory tree with /S
+            rmdir /S /Q %PI_BACKUP_DIR%
+        )
+        move %PI_PATH% %PI_BACKUP_DIR%
     )
-
+    mkdir %PI_PATH%
     REM   /I    If in doubt always assume the destination is a folder
     REM         e.g. when the destination does not exist.
     REM   /B    Copy the Symbolic link itself, not the target of the file.
@@ -25,7 +30,7 @@ if not PI_PATH == %~dp0 (
     REM   /J    Copy using unbuffered I/O. Recommended for very large files.
     REM   /Q    Do not display file names while copying.
     REM   /F    Display full source and destination file names while copying.
-    xcopy /E /I /F %~dp0 PI_PATH
+    xcopy /E /I /F %~dp0* %PI_PATH%
 )
 
 if %DEPENDENCIES_SOURCE% == "sl" (

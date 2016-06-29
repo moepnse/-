14f27d4d42efd9dac27c6a7aba6ff06b06224352
Richard Lamboj <rl@unicom.ws>
2016-06-29 05:00:42 +0200

    added test-script for SoftwareList

---
+++ software_list_test.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from libs.win.software import SoftwareList
+
+if __name__ == "__main__":
+    software_list = SoftwareList()
+    software_list["test_entry"] = {"DisplayName": "test123"}

0fb62acbffa248095d67317e400e304dfcf615b8
moepnse <rl@unicom.ws>
2016-11-16 21:44:09 +0100

    added mozilla firefox 50.0

--- config/mozilla.lua
+++ config/mozilla.lua
@@ -103,3 +103,102 @@ do
 end
 
 set_package_check_functions('firefox_esr', 'firefox_esr_is_installed', 'firefox_esr_is_upgrade_avalaible')
+
+function firefox_is_installed ()
+    return is_in_software_list([[Mozilla Firefox [0-9\.]+]])
+end
+
+function firefox_is_upgrade_avalaible ()
+    for installed_software in software_list2() do
+        if startswith(installed_software['display_name'], [[Mozilla Firefox]]) then
+            tmp = split(installed_software['display_name'], " ")
+            tmp2 = split(tmp[2], ".")
+            version_major = tonumber(tmp2[0])
+            version_minor = tonumber(tmp2[1])
+            version = tonumber(tmp2[2])
+            if version_major < 50 then
+                return true
+            end
+        end
+    end
+    return false
+end
+
+do
+    --MFFESR_IN = [[smb://\\10.0.19.10\software\mffesr.ini]]
+    MFF_INI = [[http://packages.unicom.ws/arch/noarch/mffesr.ini]]
+    MFF_EXE = "https://download-installer.cdn.mozilla.net/pub/firefox/releases/50.0/win32/en-US/Firefox%20Setup%2050.0.exe"
+    UNINSTALL_EXE = [["%PROGRAMFILES%\Mozilla Firefox\uninstall\helper.exe"]]
+    UNINSTALL_EXE = [["file://%PROGRAMFILES(X86)%\Mozilla Firefox\uninstall\helper.exe"]]
+    UNINSTALL_SILENT = "/S"
+    KILL_FF = "taskkill /F /IM firefox.exe"
+
+    KILL_FF_CMD = 
+    {
+        cmd = "file://taskkill",
+        args = {
+            {   parameter = [[/F]],
+                values = {
+                    {
+                        "",
+                        false
+                    }
+                }
+            },
+            {   parameter = [[/IM]],
+                values = {
+                    {
+                        "firefox.exe",
+                        false
+                    }
+                }
+            }
+        }
+    }
+
+    UNINSTALL_CMD = 
+    {
+        cmd = UNINSTALL_EXE,
+        args = {
+            {   parameter = [[/S]],
+                values = {
+                    {
+                        "",
+                        false
+                    }
+                }
+            }
+        }
+    }
+
+    INSTALL_CMD = 
+    {
+        cmd = MFF_EXE,
+        args = {
+            {   parameter = "/INI=",
+                values = {
+                    {
+                        MFF_INI,
+                        true
+                    }
+                }
+            },
+            {   parameter = [[/S]],
+                values = {
+                    {
+                        "",
+                        false
+                    }
+                }
+            }
+        }
+    }
+
+    install_cmds = {KILL_FF_CMD, INSTALL_CMD}
+    upgrade_cmds = {KILL_FF_CMD, INSTALL_CMD}
+    uninstall_cmds = {KILL_FF_CMD, UNINSTALL_CMD}
+
+    register_package('firefox', [[Mozilla Firefox]], "50.0.0", "1", firefox_is_installed(), firefox_is_upgrade_avalaible(), install_cmds, upgrade_cmds, uninstall_cmds, "", {})
+end
+
+set_package_check_functions('firefox', 'firefox_is_installed', 'firefox_is_upgrade_avalaible')

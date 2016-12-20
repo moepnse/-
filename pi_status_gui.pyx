56680703ac9545f51f38967da25fcc7d89fbb7c3
moepnse <rl@unicom.ws>
2016-12-20 02:29:42 +0100

    Using AnimatedImg from libs.aimg.

--- pi_status_gui.pyx
+++ pi_status_gui.pyx
@@ -13,6 +13,7 @@ import wx
 
 # application/library imports
 from package_installer import get_application_path, INSTALLING, UPGRADING, REMOVING, INSTALLED, UPGRADED, REMOVED, FAILED, UNKNOWN, SEND_STATUS, SEND_INFO, SEND_INFO_SUCCESS, SEND_INFO_WARN, SEND_INFO_ERROR, SEND_DONE
+from libs.aimg import AnimatedImg
 from c_windows_data_types cimport LPVOID, DWORD, HANDLE
 from c_windows cimport ReadFile, CreateFileW, CloseHandle, OPEN_EXISTING, GENERIC_READ, WaitForSingleObject, WAIT_OBJECT_0, WAIT_TIMEOUT, INVALID_HANDLE_VALUE, GetLastError
 from cpython.ref cimport PyObject
@@ -73,78 +74,6 @@ class EOF(Exception):
 
     def __str__(self):
         return repr(self._path)
-
-
-class AnimatedImgFrame(object):
-
-    def __init__(self, duration, img_type, img):
-        self._duration = duration
-        self._img_type = img_type
-        self._img = img
-
-    @property
-    def duration(self):
-        return self._duration
-
-    @property
-    def img_type(self):
-        return self._img_type
-
-    @property
-    def img(self):
-        return self._img
-
-
-class AnimatedImg(object):
-
-    def __init__(self, path, log):
-        self._path = path
-        self._fh = open(path, 'rb')
-        self._frames = []
-        self._index = 0
-        self._log = log
-        try:
-            while 1:
-                self._frames.append(self._get_next_frame())
-        except EOF as err:
-            pass
-        self._frame_count = len(self._frames)
-
-    def _get_frame_information(self):
-        data = self._fh.read(12)
-        if not data:
-            raise EOF(self._path)
-        # NEED FIX!
-        #self._log.log_debug(u"img_data: %s" % data)
-        return struct.unpack('IBI', data)
-
-    def _get_image(self, img_size):
-        data = self._fh.read(img_size)
-        return struct.unpack('%ds' % img_size, data)[0]
-
-    def _get_next_frame(self):
-        duration, img_type, img_size = self._get_frame_information()
-        img = self._get_image(img_size)
-        return AnimatedImgFrame(duration, img_type, img)
-
-    def get_next_frame(self):
-        if self._index == self._frame_count:
-            self._index = 0
-        frame = self._frames[self._index]
-        self._index += 1
-        return frame
-
-    def next(self):
-        return self._frames.next()
-
-    def __iter__(self):
-        return self._frames.__iter__()
-
-    def __len__(self):
-        return self._frames.__len__()
-
-    def __getitem__(self, key):
-        return self._frames[key]
 
 
 class wxAnimatedImg(AnimatedImg):

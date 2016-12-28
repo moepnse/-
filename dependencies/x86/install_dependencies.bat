cc28423a23e42dbf0112e76d7524961350ee5fe0
moepnse <rl@unicom.ws>
2016-12-28 15:43:04 +0100

    changed dependency to python version 2.7.13

--- dependencies/x86/install_dependencies.bat
+++ dependencies/x86/install_dependencies.bat
@@ -1,7 +1,7 @@
 @echo off
 
-echo Installing Python 2.7.12...
-msiexec /i %~dp0python-2.7.12.msi /qn
+echo Installing Python 2.7.13...
+msiexec /i %~dp0python-2.7.13.msi /qn
 
 echo Installing wxPython 3.0...
 %~dp0wxPython3.0-win32-3.0.2.0-py27.exe /VERYSILENT

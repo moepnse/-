5318d3d73a52e55ec5b2783a783da32c86387a6b
moepnse <rl@unicom.ws>
2017-01-11 16:50:30 +0100

    file suffix is not needed in libraries list.

--- setup.py
+++ setup.py
@@ -183,7 +183,7 @@ if "win" in sys.platform:
         sources=[os.path.join(r'libs', 'np_client.pyx')],
         extra_compile_args=compile_args,
         include_dirs = include_dirs,
-        libraries = ['Kernel32.lib'],
+        libraries = ['Kernel32'],
         library_dirs = library_dir
     )
     ]

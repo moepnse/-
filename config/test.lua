ed90bdb4ac9cd7786fc12fc0fb0a1baff20fac19
moepnse <rl@unicom.ws>
2017-01-30 02:55:50 +0100

    Added invalid test package.

--- config/test.lua
+++ config/test.lua
@@ -1,1 +1,3 @@
 register_package({package_id='rp_test', name=[[Register Package Test]], version="1.0.0", rev="1", installed=false, upgrade_available=false, install_cmds={}, upgrade_cmds={}, uninstall_cmds={}, description="", keywords={}, icon="", icon_type="", dependencies_list={{package_id = "firefox_esr", installed = false}}})
+
+register_package({package_id='rp_test2', name=[[Register Package Test2]], version="1.0.0", rev="1", installed=false, upgrade_available=false, install_cmds={}, upgrade_cmds={}, uninstall_cmds={}, description="", keywords={}, icon="", icon_type="", dependencies_list={{package_id = 1, installed = "false"}}})
